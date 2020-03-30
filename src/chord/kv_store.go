d6e64de12ddf46bb3fdb64b8efabb1def0ba45a5
zt <zhaotong_tan@mymail.sutd.edu.sg>
2020-03-30 16:40:46 +0800

    error handling

--- src/chord/kv_store.go
+++ src/chord/kv_store.go
@@ -1,18 +1,35 @@
 package chord
 
-import "errors"
+import (
+	"errors"
+)
 
 // Get file using hashed file name as key from node
 func (node *Node) get(hashedFile int) (string, error) {
-	// TODO: check if key-value pair exists
-	return node.hashTable[hashedFile], nil
+	// Check if key-value pair exists
+	// value is the value of hashedFile while ok is a bool
+	// if hashFile does not exist, ok is False
+	value, keyExists := node.hashTable[hashedFile]
+	if keyExists {
+		return value, nil
+	} else {
+		return "", errors.New("File does not exist in the table")
+	}
 }
 
 // put file using hashed file name as key into node's hashtable
 func (node *Node) put(key int, value string) error {
-	// TODO: check if key already exists
-	node.hashTable[key] = value
-	return nil
+	// Check if key-value pair exists
+	// value is the value of hashedFile while keyExists is a bool
+	// if hashFile does not exist, keyExists is False
+	value, keyExists := node.hashTable[key]
+	if keyExists {
+		return errors.New("File already exist in the table")
+	} else {
+		// Add file in hash table if it does not exist
+		node.hashTable[key] = value
+		return nil
+	}
 }
 
 // Delete removes key-value pair from storage
@@ -22,7 +39,7 @@ func (node *Node) delete(key int) error {
 		delete(node.hashTable, key)
 		return nil
 	}
-	return errors.New("Key already does not exist in table")
+	return errors.New("Key already does not exist in the table")
 }
 
 // TransferKeys allow reassignment of keys on node join/fail
