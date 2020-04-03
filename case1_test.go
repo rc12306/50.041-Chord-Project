3bb83990f2e56a3481a1809558665cf20f78d497
kohzhixin <1002914@mymail.sutd.edu.sg>
2020-04-03 14:45:36 +0800

    created a node

--- case1_test.go
+++ case1_test.go
@@ -1,16 +1,42 @@
 package main
 
-import "testing"
+import (
+  "testing"
+  "fmt"
+  "chord/src/chord"
+)
 
-// prototype to demonstrate "testing" with scan_ring.go
-func TestScanRing(t *testing.T) {
-  ipInRing := CheckRing()
-  // check if ipInRing is empty
-  if len(ipInRing) == 0 {
-    t.Errorf("No nodes found in chord ring )-:")
+func Test1(t *testing.T) {
+  // init User IP info
+  IP := chord.GetOutboundIP()
+  IP_str := fmt.Sprint(ip2Long(IP))
+  ID := chord.Hash(IP_str)
+
+  // create a node
+  chord.ChordNode = &chord.Node{
+    Identifier: -1,
   }
-}
+  chord.ChordNode.IP = IP
+	chord.ChordNode.Identifier = ID
+	go node_listen(IP)
+	chord.ChordNode.CreateNodeAndJoin(nil)
+	fmt.Println("Created chord network (" + IP + ") as " + fmt.Sprint(ID) + ".")
+  chord.ChordNode.PrintNode()
 
-// TODO:
-// Debug errors found when using
-// go test -run TestScanRing
+  // remoteNode_IP :=                             //String of IP
+	// remoteNode_IP_str := fmt.Sprint(ip2Long(remoteNode_IP)) //String of decimal IP
+	// remoteNode_ID := chord.Hash(remoteNode_IP_str)          //Hash of decimal IP
+	// remoteNode := &chord.RemoteNode{
+	// 	Identifier: remoteNode_ID,
+	// 	IP:         remoteNode_IP,
+	// }
+	// chord.ChordNode.IP = IP
+	// chord.ChordNode.Identifier = ID
+  //
+	// go node_listen(IP)
+	// chord.ChordNode.CreateNodeAndJoin(remoteNode)
+  //
+	// fmt.Println("remoteNode is (" + remoteNode_IP + ") " + fmt.Sprint(remoteNode_ID) + ".")
+	// fmt.Println("Joined chord network (" + IP + ") as " + fmt.Sprint(ID) + ". ")
+
+}
