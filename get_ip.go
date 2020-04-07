e831b577ccc87329520829aa766ae00a89f4f204
Loo Tan Rong <lootr5858@gmail.com>
2020-04-08 00:30:55 +0800

    commented main func

---
+++ get_ip.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String()
+}
+
+// func main() {
+
+// 	fmt.Println(GetOutboundIP())
+
+// }
