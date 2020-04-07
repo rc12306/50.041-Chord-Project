644cd19e91dca4119663060dfc8b41fe8dde25d2
Loo Tan Rong <lootr5858@gmail.com>
2020-04-07 21:00:03 +0800

    fixed rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 docker-compose build
 docker-compose up -d --scale node=5
-
-for i in 1 2 3 4 5
-do
-  gnome-terminal --command "bash -c 'docker exec -it 50041chordproject_node_$i bash; $SHELL'"
-done
