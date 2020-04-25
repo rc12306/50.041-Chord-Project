df3680d5d185b0516796f0e8821931e857b93609
kohzhixin <1002914@mymail.sutd.edu.sg>
2020-04-25 21:28:23 +0800

    added test3

---
+++ test3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# rebuild
+docker-compose build
+docker-compose up -d --scale node=3
+
+# this script runs Test1 in 3 Docker containers
+# extract all running container id
+nodes=$(bash -c "docker ps -a -q")
+
+# bash each container in new window
+for node in $nodes
+do
+    gnome-terminal --command "bash -c 'docker exec -it $node go test -run Test3; $SHELL'"
+done
